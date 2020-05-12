8a3788f632e6a99bd0e839d09cfae4a16e478db0
kimhayoung2 <60481275+kimhayoung2@users.noreply.github.com>
2020-05-12 15:42:01 -0400

    Create testing_slideshow.js

---
+++ testing_slideshow.js
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<!--
+  Prologue by HTML5 UP
+  html5up.net | @ajlkn
+  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+  <head>
+    <title>Ha Young Kim | Portfolio</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="testing_slideshow.css" />
+
+  </head>
+  <body>
+    <div class="slideshowContainer">
+      
+    <!-- Replace the image 'src' with the images in your project.
+    Javascript is set up so that you can add as many images as you like, but make sure that you match the number of 'circle' span elements (below) to the number of images -->
+      <div class="mySlides">
+        <div class="numbertext">1 / 3</div><img class="imageSlides" src="http://s1.picswalls.com/wallpapers/2015/09/20/beautiful-hd-wallpaper-2015_111526537_269.jpg" alt="beach side city view"><div class="text">Caption Text</div></div>
+      
+      <div class="mySlides">
+        <div class="numbertext">2 / 3</div><img class="imageSlides" src="http://4.bp.blogspot.com/-oxlezteeOII/TfiTImj4RlI/AAAAAAAAA1k/UAgctmU5VZo/s1600/Widescreen+Unique+And+Beautiful+Photography+%25284%2529.jpg" alt="leaf on the ground"><div class="text">Caption Two</div></div>
+        
+      <div class="mySlides">
+        <div class="numbertext">3 / 3</div><img class="imageSlides" src="https://i.pinimg.com/originals/cf/ed/a0/cfeda03d01da4b779093c9d928c01dee.jpg" alt="lake surrounded by mountains"><div class="text">Caption Three</div></div>
+      
+    </div>
+      
+      <span id ="leftArrow" class="slideshowArrow">&#8249;</span>
+      <span id ="rightArrow" class="slideshowArrow">&#8250;</span>
+      
+      <div style="text-align:center">
+          <span class="dot"></span> 
+          <span class="dot"></span> 
+          <span class="dot"></span> 
+      </div>
+
+  <body> 
+
+   <script src= "testing_slideshow2.js"></script>
